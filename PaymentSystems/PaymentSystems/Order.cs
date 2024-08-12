5a0f0e3191f9814710c9844fc654eb515a0c2301
Vitalii <lennab@mail.ru>
2024-08-12 13:43:47 +0300

    Fix

--- PaymentSystems/PaymentSystems/Order.cs
+++ PaymentSystems/PaymentSystems/Order.cs
@@ -1,10 +1,22 @@
-﻿namespace PaymentSystems
+﻿using System;
+
+namespace PaymentSystems
 {
     public class Order
     {
         public readonly int Id;
         public readonly int Amount;
 
-        public Order(int id, int amount) => (Id, Amount) = (id, amount);
+        public Order(int id, int amount)
+        {
+            if (id <= 0) 
+                throw new ArgumentOutOfRangeException(nameof(id));
+
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            Id = id;
+            Amount = amount;
+        }
     }
 }
--- PaymentSystems/PaymentSystems/PaymentSystem.cs
+++ PaymentSystems/PaymentSystems/PaymentSystem.cs
@@ -4,21 +4,19 @@ namespace PaymentSystems
 {
     public abstract class PaymentSystem : IPaymentSystem
     {
-        private IHashCreator _hashCreator;
-
-        public PaymentSystem(IHashCreator hashCreator)
+        protected PaymentSystem(IHashCreator hashCreator)
         {
-            _hashCreator = hashCreator ?? throw new ArgumentNullException(nameof(hashCreator));
+            HashCreator = hashCreator ?? throw new ArgumentNullException(nameof(hashCreator));
         }
 
-        protected IHashCreator HashCreator => _hashCreator;
+        protected IHashCreator HashCreator { get; }
+
+        public abstract string GetPayingLink(Order order);
 
         protected void ThrowEcxeptionIfArgumentNull(Order order)
         {
             if (order == null) 
                 throw new ArgumentNullException(nameof(order));
         }
-
-        public abstract string GetPayingLink(Order order);
     }
 }
